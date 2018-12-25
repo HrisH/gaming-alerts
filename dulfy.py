cce93dab4918846a60dffa56f1d2c494b0ce27d3
Hrishikesh JUVALE <hrishikeshjuvale@gmail.com>
2018-12-25 20:33:55 +0530

    Removed unnecessary comments and code

--- dulfy.py
+++ dulfy.py
@@ -1,34 +1,17 @@
 #!/usr/bin/python2
 # -*- coding: utf-8 -*-
-##import sys
-##sys.path.insert(0, '/home/ubuntu/PythonStorage/lib') # add '/home/ubuntu/PythonStorage/lib' to the Python path at runtime
-##sys.path.insert(0, 'C:\\Python Storage\\lib') # add 'C:\\Python Storage\\lib' to the Python path at runtime
-##
-##import commonlibrary
-##import pgsqldb
 import requests
-
 import re
-
 import traceback
 import sys
 import os
 import shutil
-
 from lxml.html import document_fromstring, tostring
-
 from time import strftime
-
 from bs4 import BeautifulSoup
 
 feed_name = "dulfy"
 feed_url = "http://dulfy.net/"
-
-embargo_table_name = "embargo.embargo_celex"
-
-image_directory = "C:\\Specific Tasks\\Embargo Websites\\Images\\"
-
-domain = "http://eur-lex.europa.eu/"
 
 ######################################################################
 ##Project Name - dulfy
@@ -39,13 +22,10 @@ domain = "http://eur-lex.europa.eu/"
 ######################################################################
 
 def main():
-    print "Start Time -> ", strftime("%Y-%m-%d %H:%M:%S")
+    print("Start Time -> ", strftime("%Y-%m-%d %H:%M:%S"))
 
-##    # Initiate the Database Connection by opening the connection and then Preparing a cursor object to use queries
-##    db, cursor = pgsqldb.initiateDatabaseConnection()
-
-    content = requests.get(feed_url, False).content
-    ##print content
+    content = requests.get(url=feed_url, verify=False).content
+    ##print(content)
 
     root = document_fromstring(content)
 
@@ -56,43 +36,15 @@ def main():
     for feature_posts_div in feature_posts_div_list:
         if str(feature_posts_div.find(".//h2").text) == "Guild Wars 2":
             ul = feature_posts_div.find(".//ul[@class='feature-posts-list']")
-##            print tostring(ul) #provides html of the current tag as well as all inner tags till the last level
-##            print ul.text_content() #provides text of the current as well as all inner tags till the last level
+##            print(tostring(ul)) #provides html of the current tag as well as all inner tags till the last level
+##            print(ul.text_content()) #provides text of the current as well as all inner tags till the last level
             li_list = ul.findall('.//li')
 ##            print li_list  #provides list of elements
             for li in li_list:
-##                print tostring(li) #provides html of the current tag as well as all inner tags till the last level
-                print li.text_content() #provides text of the current as well as all inner tags till the last level
-##                print li.text #provides text of the current tag
-##                print u"".join([x for x in li.itertext()]).strip()
-##                a_list = li.findall('.//a')
-##                for a in a_list:
-##                    if a.attrib.get("class") == "feature-posts-title":
-##                        print a.attrib.get("href")
-##                print li.find(".//a[@class='feature-posts-title']").attrib.get("href") #print the href values of a tag with class = feature-posts-title
-##                    print li.text
+##                print(tostring(li)) #provides html of the current tag as well as all inner tags till the last level
+                print(li.text_content()) #provides text of the current as well as all inner tags till the last level
 
-
-
-##    soup = BeautifulSoup(content, "lxml")
-##
-##    page_content_div = soup.find('div', {'class':'page-content'}) #finding a div tag with class = page-content
-##
-##    feature_posts_div_list = page_content_div.find_all('div', {'class':'widget feature-posts'})
-##
-##    for feature_posts_div in feature_posts_div_list:
-##        if '<h2 class="widgettitle">Guild Wars 2</h2>' in str(feature_posts_div):
-##            ul = feature_posts_div.find('ul', {'class':'feature-posts-list'})
-##            li_list = ul.find_all('li')
-####            print li_list  #provides html of the current tag as well as all inner tags till the last level
-##            for li in li_list:
-##                print li #provides html of the current tag as well as all inner tags till the last level
-##                print li.text #provides text of the current as well as all inner tags till the last level
-
-##    # disconnect from server
-##    db.close()
-
-    print "End Time -> ", strftime("%Y-%m-%d %H:%M:%S")
+    print("End Time -> ", strftime("%Y-%m-%d %H:%M:%S"))
 
 if __name__ == "__main__":
     main()
--- playstation_store.py
+++ playstation_store.py
@@ -1,31 +1,17 @@
 #!/usr/bin/python2
 # -*- coding: utf-8 -*-
-##import sys
-##sys.path.insert(0, '/home/ubuntu/PythonStorage/lib') # add '/home/ubuntu/PythonStorage/lib' to the Python path at runtime
-##sys.path.insert(0, 'C:\\Python Storage\\lib') # add 'C:\\Python Storage\\lib' to the Python path at runtime
-##sys.path.insert(0, '/home/pythonstorage/lib') # add '/home/ubuntu/PythonStorage/lib' to the Python path at runtime
-##
-##import commonlibrary
-##import pgsqldb
-
 import requests
-
 import re
-
 import traceback
 import sys
 import os
 import shutil
-
 from lxml.html import document_fromstring, tostring
-
 from time import strftime
-
 from bs4 import BeautifulSoup
 
 feed_name = "playstation_store"
 feed_url = "https://store.playstation.com/fr-be/home/games"
-
 domain = "https://store.playstation.com"
 
 ######################################################################
@@ -37,43 +23,37 @@ domain = "https://store.playstation.com"
 ######################################################################
 
 def main():
-    print "Start Time -> ", strftime("%Y-%m-%d %H:%M:%S")
-
-##    # Initiate the Database Connection by opening the connection and then Preparing a cursor object to use queries
-##    db, cursor = pgsqldb.initiateDatabaseConnection()
+    print("Start Time -> ", strftime("%Y-%m-%d %H:%M:%S"))
 
     content = requests.get(feed_url, False).content
-##    print content
+##    print(content)
 
     soup = BeautifulSoup(content, "lxml")
 
     left_panel_div = soup.find('div', {'class':'left-panel'}) #finding a div tag with class = left-panel
 
     ps_vita_page_url = domain + left_panel_div.find('a', text='PS Vita™')['href']
-##    print ps_vita_page
+##    print(ps_vita_page)
 
     current_page = 1
-##    ps_vita_page_content = requests.get(ps_vita_page_url, False).content
-
-##    page_soup = BeautifulSoup(ps_vita_page_content, "lxml")
 
     updated_ps_vita_page_url = ps_vita_page_url + "/" + str(current_page) + "?gameContentType=games&platform=vita"
 
-    ps_vita_page_content = requests.get(updated_ps_vita_page_url, False).content
-##    print ps_vita_page_content
+    ps_vita_page_content = requests.get(url=updated_ps_vita_page_url, verify=False).content
+##    print(ps_vita_page_content)
 
     page_soup = BeautifulSoup(ps_vita_page_content, "lxml")
 
-##    print page_soup.find('a', {'class': 'paginator-control__end paginator-control__arrow-navigation internal-app-link ember-view'})['href']
+##    print(page_soup.find('a', {'class': 'paginator-control__end paginator-control__arrow-navigation internal-app-link ember-view'})['href'])
 
     no_of_pages = int(re.search("\/\d+", page_soup.find('a', {'class': 'paginator-control__end paginator-control__arrow-navigation internal-app-link ember-view'})['href'], re.I|re.M|re.U).group(0).replace("/", ""))
 
-##    print no_of_pages
+##    print(no_of_pages)
 
     while current_page < no_of_pages + 1:
         updated_ps_vita_page_url = ps_vita_page_url + "/" + str(current_page) + "?gameContentType=games&platform=vita"
 
-        ps_vita_page_content = requests.get(updated_ps_vita_page_url, False).content
+        ps_vita_page_content = requests.get(url=updated_ps_vita_page_url, verify=False).content
 
         page_soup = BeautifulSoup(ps_vita_page_content, "lxml")
 
@@ -85,14 +65,11 @@ def main():
                 
                 promo_id = promo_links_a['id']
 
-                print promo_id, promo_link
+                print(promo_id, promo_link)
 
         current_page += 1
 
-##    # disconnect from server
-##    db.close()
-
-    print "End Time -> ", strftime("%Y-%m-%d %H:%M:%S")
+    print("End Time -> ", strftime("%Y-%m-%d %H:%M:%S"))
 
 if __name__ == "__main__":
     main()
